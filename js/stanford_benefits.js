13011c58e57bec0064c163facad638bc63f1f0a6
Bill Minor <minorwm@stanford.edu>
2019-07-31 13:01:04 -0500

    CAW-861: Adding css and js dirs

---
+++ js/stanford_benefits.js
@@ -0,0 +1,23 @@
+(function ($) {
+
+/**
+ * Drupal attach behaviour.
+ */
+
+  Drupal.behaviors.stanford_benefits = {
+    attach: function (context, settings) {
+      $( document ).ready(function() {
+        // If no checkboxes are selected, hide results to fix view bug.
+        if (!$('input[name="term_medical_active_benefit[]"]:checked').length &&
+          !$('input[name="term_medical_preretirees_benefit[]"]:checked').length &&
+          !$('input[name="term_medical_retirees_benefit[]"]:checked').length &&
+          !$('input[name="term_dental_active_benefit[]"]:checked').length) {
+            $(".view-caw-benefit-comparison-tool .view-content").css("display", "none");
+        }
+        else {
+          $(".view-caw-benefit-comparison-tool .view-content").css("display", "block");
+        }
+      });
+    }
+  };
+})(jQuery);
