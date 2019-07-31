2e7e14e8f27188a14f11bf35d1da64bbba5cdc63
Bill Minor <minorwm@stanford.edu>
2019-07-31 13:43:14 -0500

    CAW-861: Adding the gruntfile, package.json, and gitignore.

---
+++ Gruntfile.js
@@ -0,0 +1,25 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    sass: {
+      options: {
+        lineNumbers: true,
+        sourcemap: 'none'
+      },
+      dist: {
+        files: {
+          'css/stanford-benefits.css': 'scss/stanford-benefits.scss'
+        }
+      }
+    },
+    watch: {
+      css: {
+        files: '**/*.scss',
+        tasks: ['sass']
+      }
+    }
+  });
+  grunt.loadNpmTasks('grunt-sass');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.registerTask('default', ['watch']);
+}
