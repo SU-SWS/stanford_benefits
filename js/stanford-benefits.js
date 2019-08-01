1370f9d9fffee2021f576d4d527c3044657c772d
Bill Minor <minorwm@stanford.edu>
2019-08-01 16:20:01 -0500

    CAW-861: Fixing edge case bugs with hiding and showing in jquery.

--- js/stanford-benefits.js
+++ js/stanford-benefits.js
@@ -2,12 +2,12 @@
 /**
  * Drupal attach behaviour.
  */
-
   Drupal.behaviors.stanford_benefits = {
     attach: function (context, settings) {
-      $( document ).ready(function() {
+      $( document).ready(function() {
         // Setup the variables we will use here.
         var CompTool = '.view-caw-benefit-comparison-tool';
+        var SubmitButton = '#edit-submit-caw-benefit-comparison-tool';
         var CompToolBlock = '#views-exposed-form-caw-benefit-comparison-tool-block';
         var MedicalActive = '#edit-term-medical-active-benefit-wrapper';
         var MedicalPreretirees = '#edit-term-medical-preretirees-benefit-wrapper';
@@ -41,105 +41,113 @@
           }
         }
         // Show the Medical/Active exposed filters
-        $(MedicalActive + ' label', context).click(function() {
+        $(MedicalActive + ' label:first-child', context).click(function() {
           ShowSection(MedicalActive);
         });
 
         // Show the Medical/Pre-retirees exposed filters
-        $(MedicalPreretirees + ' label', context).click(function() {
+        $(MedicalPreretirees + ' label:first-child', context).click(function() {
           ShowSection(MedicalPreretirees);
         });
 
         // Show the Medical/Retirees exposed filters
-        $(MedicalRetirees + ' label', context).click(function() {
+        $(MedicalRetirees + ' label:first-child', context).click(function() {
           ShowSection(MedicalRetirees);
         });
 
         // Show the Dental/Active exposed filters
-        $(DentalActive + ' label', context).click(function() {
+        $(DentalActive + ' label:first-child', context).click(function() {
           ShowSection(DentalActive);
         });
 
         // Show the Dental/Retirees exposed filters
-        $(DentalRetirees + ' label', context).click(function() {
+        $(DentalRetirees + ' label:first-child', context).click(function() {
           ShowSection(DentalRetirees);
         });
 
         // Function to move some things around for UI beauty.
         function MoveViewHTMLElements() {
-          $(MedicalActive).before( $('#medical-plan-header') );
-          $(DentalActive).before( $('#dental-plan-header') );
-          $('.view-header' + ' > p').remove();
+          $(MedicalActive).before( $("#medical-plan-header") );
+          $(DentalActive).before( $("#dental-plan-header") );
+          $('.view-header' + ' > p', context).remove();
           $(CompTool + ' .view-header').before( $(CompTool + ' .view-header' + ' h2.plan-rates-title') );
-          $(MedicalActive + ' .views-widget').before( $(MedicalActive + ' .description') );
-          $(MedicalPreretirees + ' .views-widget').before( $(MedicalPreretirees + ' .description') );
-          $(MedicalRetirees + ' .views-widget').before( $(MedicalRetirees + ' .description') );
-          $(DentalActive + ' .views-widget').before( $(DentalActive + ' .description') );
-          $(DentalRetirees + ' .views-widget').before( $(DentalRetirees + ' .description') );
+          $(MedicalActive + ' .views-widget', context).before( $(MedicalActive + ' .description') );
+          $(MedicalPreretirees + ' .views-widget', context).before( $(MedicalPreretirees + ' .description') );
+          $(MedicalRetirees + ' .views-widget', context).before( $(MedicalRetirees + ' .description') );
+          $(DentalActive + ' .views-widget', context).before( $(DentalActive + ' .description') );
+          $(DentalRetirees + ' .views-widget', context).before( $(DentalRetirees + ' .description') );
         }
 
         // Function to hide all the different exposed filters.
         function HideAll() {
-          $(MedicalActive + ' .description').css('display', 'none');
-          $(MedicalActive + ' .views-widget').css('display', 'none');
+          $(MedicalActive + ' .description', context).css('display', 'none');
+          $(MedicalActive + ' .views-widget', context).css('display', 'none');
 
-          $(MedicalPreretirees + ' .description').css('display', 'none');
-          $(MedicalPreretirees + ' .views-widget').css('display', 'none');
+          $(MedicalPreretirees + ' .description', context).css('display', 'none');
+          $(MedicalPreretirees + ' .views-widget', context).css('display', 'none');
 
-          $(MedicalRetirees + ' .description').css('display', 'none');
-          $(MedicalRetirees + ' .views-widget').css('display', 'none');
+          $(MedicalRetirees + ' .description', context).css('display', 'none');
+          $(MedicalRetirees + ' .views-widget', context).css('display', 'none');
 
-          $(DentalActive + ' .description').css('display', 'none');
-          $(DentalActive + ' .views-widget').css('display', 'none');
+          $(DentalActive + ' .description', context).css('display', 'none');
+          $(DentalActive + ' .views-widget', context).css('display', 'none');
 
-          $(DentalRetirees + ' .description').css('display', 'none');
-          $(DentalRetirees + ' .views-widget').css('display', 'none');
+          $(DentalRetirees + ' .description', context).css('display', 'none');
+          $(DentalRetirees + ' .views-widget', context).css('display', 'none');
 
-          $(CompTool + ' .view-empty').css('display', 'none');
+          $(CompTool + ' .view-empty', context).css('display', 'none');
 
-          $(CompToolBlock + ' .views-submit-button').css('display', 'none');
+          $(SubmitButton, context).css('display', 'none');
         }
 
         // Function to show the right section when clicked.
         function ShowSection(section) {
-          if ($(section + ' .description').is(':visible')) {
-            $(section + ' .description').css('display', 'none');
-            $(section + ' .views-widget').css('display', 'none');
-            $(CompTool + ' .view-empty').css('display', 'none');
-            $(CompToolBlock + ' .views-submit-button').css('display', 'none');
+          if ($(section + ' .description', context).is(':visible')) {
+            $(section + ' .description', context).css('display', 'none');
+            $(section + ' .views-widget', context).css('display', 'none');
+            $(CompTool + ' .view-empty', context).css('display', 'none');
+            $(SubmitButton, context).css('display', 'none');
           }
           else {
-            $(section + ' .description').css('display', 'block');
-            $(section + ' .views-widget').css('display', 'block');
-            $(CompTool + ' .view-empty').css('display', 'block');
-            $(CompToolBlock + ' .views-submit-button').css('display', 'block');
+            $(section + ' .description', context).css('display', 'block');
+            $(section + ' .views-widget', context).css('display', 'block');
+            $(CompTool + ' .view-empty', context).css('display', 'block');
+            $(SubmitButton, context).css('display', 'block');
           }
 
           if ( section !== MedicalActive) {
-            $(MedicalActive + ' .description').css('display', 'none');
-            $(MedicalActive + ' .views-widget').css('display', 'none');
+            $(MedicalActive + ' .description', context).css('display', 'none');
+            $(MedicalActive + ' .views-widget', context).css('display', 'none');
           }
           if ( section !== MedicalPreretirees) {
-            $(MedicalPreretirees + ' .description').css('display', 'none');
-            $(MedicalPreretirees + ' .views-widget').css('display', 'none');
+            $(MedicalPreretirees + ' .description', context).css('display', 'none');
+            $(MedicalPreretirees + ' .views-widget', context).css('display', 'none');
           }
           if ( section !== MedicalRetirees) {
-            $(MedicalRetirees + ' .description').css('display', 'none');
-            $(MedicalRetirees + ' .views-widget').css('display', 'none');
+            $(MedicalRetirees + ' .description', context).css('display', 'none');
+            $(MedicalRetirees + ' .views-widget', context).css('display', 'none');
           }
           if ( section !== DentalActive) {
-            $(DentalActive + ' .description').css('display', 'none');
-            $(DentalActive + ' .views-widget').css('display', 'none');
+            $(DentalActive + ' .description', context).css('display', 'none');
+            $(DentalActive + ' .views-widget', context).css('display', 'none');
           }
           if ( section !== DentalRetirees) {
-            $(DentalRetirees + ' .description').css('display', 'none');
-            $(DentalRetirees + ' .views-widget').css('display', 'none');
+            $(DentalRetirees + ' .description', context).css('display', 'none');
+            $(DentalRetirees + ' .views-widget', context).css('display', 'none');
           }
+        }
+
+        function IsSectionChecked(term) {
+          var re = /\-/gi;
+          var tmp = term.replace('#edit-', '');
+          var underscoreme = tmp.replace('-wrapper', '');
+          var underscores = underscoreme.replace(re, '_');
+          return IsChecked(underscores);
         }
 
         // Function to tell if anything is check on the exposed fields.
         function IsChecked(term) {
-          return $('input[name="' + term + '[]"]:checked').prop("checked");
+          return $('input[name="' + term + '[]"]:checked').length;
         }
 
         // Are there any checkboxes selected.
@@ -162,28 +170,28 @@
 
         // Function to show and hide the right section for the views exposed filters.
         function ShowFormFor(section) {
-          $('#' + section + ' .description').css('display', 'block');
-          $('#' + section + ' .views-widget').css('display', 'block');
+          $('#' + section + ' .description', context).css('display', 'block');
+          $('#' + section + ' .views-widget', context).css('display', 'block');
 
           if ( section !== MedicalActive) {
-            $(MedicalActive + ' .description').css('display', 'none');
-            $(MedicalActive + ' .views-widget').css('display', 'none');
+            $(MedicalActive + ' .description', context).css('display', 'none');
+            $(MedicalActive + ' .views-widget', context).css('display', 'none');
           }
           if ( section !== MedicalPreretirees) {
-            $(MedicalPreretirees + ' .description').css('display', 'none');
-            $(MedicalPreretirees + ' .views-widget').css('display', 'none');
+            $(MedicalPreretirees + ' .description', context).css('display', 'none');
+            $(MedicalPreretirees + ' .views-widget', context).css('display', 'none');
           }
           if ( section !== MedicalRetirees) {
-            $(MedicalRetirees + ' .description').css('display', 'none');
-            $(MedicalRetirees + ' .views-widget').css('display', 'none');
+            $(MedicalRetirees + ' .description', context).css('display', 'none');
+            $(MedicalRetirees + ' .views-widget', context).css('display', 'none');
           }
           if ( section !== DentalActive) {
-            $(DentalActive + ' .description').css('display', 'none');
-            $(DentalActive + ' .views-widget').css('display', 'none');
+            $(DentalActive + ' .description', context).css('display', 'none');
+            $(DentalActive + ' .views-widget', context).css('display', 'none');
           }
           if ( section !== DentalRetirees) {
-            $(DentalRetirees + ' .description').css('display', 'none');
-            $(DentalRetirees + ' .views-widget').css('display', 'none');
+            $(DentalRetirees + ' .description', context).css('display', 'none');
+            $(DentalRetirees + ' .views-widget', context).css('display', 'none');
           }
         }
       });
